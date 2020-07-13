391ff962388f86c2674f2dcc52c0005a1293fc06
柏葉　祐輝 <yuki.kashiwaba@gmail.com>
2020-07-13 17:52:46 +0900

    DBにTrainLogを追加

--- WebAppProject/main/models.py
+++ WebAppProject/main/models.py
@@ -10,6 +10,16 @@ class TaskType(enum.Enum):
     def choices(cls):
         return [(m.value, m.name) for m in cls]
 
+# Enum Class
+class ProcessStatus(enum.Enum):
+    completed = 1
+    processing = 2
+    error = 3
+
+    @classmethod
+    def choices(cls):
+        return [(m.value, m.name) for m in cls]
+
 # Create your models here.
 class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -27,3 +37,18 @@ class Dataset(models.Model):
     created_at = models.DateTimeField('date published')
     def __str__(self):
         return self.name
+
+class TrainLog(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    train_dataset = models.ForeignKey(Dataset, on_delete=models.SET_NULL, null=True, related_name = "train_dataset")
+    val_dataset = models.ForeignKey(Dataset, on_delete=models.SET_NULL, null=True, related_name = "val_dataset")
+    task_type = models.IntegerField(
+        choices=TaskType.choices()
+    )
+    status = models.IntegerField(
+        choices=ProcessStatus.choices()
+    )
+    created_at = models.DateTimeField('date published')
+    def __str__(self):
+        return self.description
